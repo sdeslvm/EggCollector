a9425e265b62c426f464b49b7d2e8937b955b31d
Pavel Ivanov <p.ivanov@citydrive.ru>
2025-04-02 22:00:29 +0300

    Initial Commit

---
+++ foxgame/foxgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  foxgameApp.swift
+//  foxgame
+//
+//  Created by Pavel Ivanov on 02.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct foxgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
